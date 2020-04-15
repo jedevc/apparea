ce2258c8c049ed8776e4a91b62c85e9a4c3e778f
Justin Chadwell <jedevc@gmail.com>
2020-04-15 12:35:39 +0100

    Get https working!

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -14,11 +14,8 @@ provider "gandi" {
   key = var.gandi_key
 }
 
-data "gandi_livedns_domain" "apparea_dev" {
-  name = "apparea.dev"
-}
-
-data "hcloud_ssh_keys" "all_keys" {
+provider "acme" {
+  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
 }
 
 data "template_file" "cloudinit" {
@@ -26,7 +23,12 @@ data "template_file" "cloudinit" {
 
   vars = {
     ip_address = hcloud_floating_ip.master.ip_address
+    tls_cert = acme_certificate.certificate.certificate_pem
+    tls_key = acme_certificate.certificate.private_key_pem
   }
+}
+
+data "hcloud_ssh_keys" "all_keys" {
 }
 
 resource "hcloud_server" "web" {
@@ -50,12 +52,38 @@ resource "hcloud_floating_ip_assignment" "master" {
   server_id = hcloud_server.web.id
 }
 
+data "gandi_livedns_domain" "apparea_dev" {
+  name = "apparea.dev"
+}
+
 resource "gandi_livedns_record" "apparea_record" {
   zone = data.gandi_livedns_domain.apparea_dev.name
-  name = "apparea"
+  name = "@"
   type = "A"
   ttl = 3600
   values = [
     hcloud_floating_ip.master.ip_address
   ]
+}
+
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+}
+
+resource "acme_registration" "reg" {
+  account_key_pem = tls_private_key.private_key.private_key_pem
+  email_address   = "jedevc@gmail.com"
+}
+
+resource "acme_certificate" "certificate" {
+  account_key_pem           = acme_registration.reg.account_key_pem
+  common_name               = "*.apparea.dev"
+
+  dns_challenge {
+    provider = "gandiv5"
+
+    config = {
+      GANDIV5_API_KEY = var.gandi_key
+    }
+  }
 }
